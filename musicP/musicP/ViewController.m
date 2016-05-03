fe482263f0fb1c00c2013f445696c6c9fb4a1ee0
yuanhaohua <yuanhaohua123@qq.com>
2016-05-03 20:49:39 +0800

    yuan

---
+++ musicP/musicP/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  musicP
+//
+//  Created by yuan on 16/5/3.
+//  Copyright © 2016年 yuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"dxydxy");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
